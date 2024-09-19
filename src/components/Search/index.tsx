5362448bfd06a47b3cf1332eb1060e71551c80cb
Mark Carson <markcarson121@gmail.com>
2024-09-19 16:54:20 -0300

    close search bar if outside click

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -1,3 +1,5 @@
+// React
+import { useEffect, useRef } from "react";
 // Third party
 import clsx from "clsx";
 import { Transition } from "@headlessui/react";
@@ -12,22 +14,43 @@ type SearchProps = {
 
 export function Search(props: SearchProps) {
   const { isOpen, onClose, className } = props;
+  const searchRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function onClickOutside(event: MouseEvent) {
+      if (
+        searchRef.current &&
+        !searchRef.current.contains(event.target as Node)
+      ) {
+        onClose();
+      }
+    }
+
+    if (isOpen) {
+      document.addEventListener("mousedown", onClickOutside);
+    } else {
+      document.removeEventListener("mousedown", onClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", onClickOutside);
+    };
+  }, [isOpen, onClose]);
 
   return (
     <Transition show={isOpen}>
       <div
+        ref={searchRef}
         className={clsx(
-          "absolute top-[96px] w-screen bg-white px-8 h-[96px] shadow-xl transition ease-in-out flex justify-center items-center",
-          className,
-          // Entering styles
+          "absolute lg:top-24 w-screen bg-white px-[12px] lg:px-8 h-16 lg:h-24 shadow-xl transition ease-in-out flex justify-center items-center",
           "data-[enter]:duration-500 data-[enter]:data-[closed]:-translate-y-full data-[closed]:opacity-0",
-          // Leaving styles
-          "data-[leave]:duration-500 data-[leave]:data-[closed]:-translate-y-full"
+          "data-[leave]:duration-500 data-[leave]:data-[closed]:-translate-y-full",
+          className
         )}
       >
         <div className="flex w-full justify-between items-center">
-          <div className="w-6"></div>
-          <div className="flex border-b-4 border-gray-200 pb-2 w-1/2 text-lg">
+          <div className="w-6 hidden lg:block"></div>
+          <div className="flex items-center border-b-2 lg:border-b-4 border-gray-200 lg:pb-2 w-full lg:w-1/2 text-base lg:text-lg h-10">
             <div className="px-4">
               <Icon icon="Search" height={24} width={24} />
             </div>
@@ -36,7 +59,7 @@ export function Search(props: SearchProps) {
               placeholder="What are you looking for?"
             />
           </div>
-          <button onClick={onClose}>
+          <button className="hidden lg:block" onClick={onClose}>
             <Icon icon="Close" height={24} width={24} />
           </button>
         </div>
